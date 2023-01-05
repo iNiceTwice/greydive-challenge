afa7773b562d126e85d724c4fa1da0962efd3284
NiceTwice <onlynicetwice@gmail.com>
2023-01-05 16:48:28 -0300

    adding form component

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import formItems from "../db/db.json"
+
+const Form = () => {
+
+    console.log(formItems)
+    return ( 
+        <>
+            <form className="flex flex-col">
+
+            </form>
+        </>
+     );
+}
+ 
+export default Form;
