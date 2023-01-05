7d7704484320afea8c6d72515fa32740f2dc37c1
NiceTwice <onlynicetwice@gmail.com>
2023-01-05 16:49:19 -0300

    adding input component

---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+const Input = () => {
+    return ( 
+        <>
+            <label className="w-full"></label>
+            <input className="w-full" />
+        </>
+     );
+}
+ 
+export default Input;
